04b10d604cf9582fad8905a2a80c0c30bd6323f8
z0ok <44017357+z0ok@users.noreply.github.com>
2023-09-07 11:10:36 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+### Current settings. If you want to change port or user - change here.
+### IF you want to add more params to node_exporter executable - edit node_exporter.service file
 USER="node_exporter"
+TARGET_PORT="9100"
 
 if [ "$EUID" -ne 0 ]
   then echo "[!] Please run as root."
   exit
 fi
 
-echo "[*] Creating user $USER"
+echo "[*] Checking user $USER"
 
 if id $USER &>/dev/null; then
     echo "[+] User $USER already exists"
@@ -21,11 +24,17 @@ else
     fi
 fi
 
-if lsof -Pi :9100 -sTCP:LISTEN -t >/dev/null ; then
-    echo "[-] 9100 port is already used. Cannot bind there!"
+echo '[*] Checking port'
+if [ $TARGET_PORT != "9100" ]; then
+    sed -i "s/--web.listen-address=:9100/--web.listen-address=:$TARGET_PORT/" node_exporter.service
+else
+fi
+
+if lsof -Pi :$TARGET_PORT -sTCP:LISTEN -t >/dev/null ; then
+    echo "[-] $TARGET_PORT port is already used. Cannot bind there!"
     exit
 else
-    echo "[+] Port is available"
+    echo "[+] $TARGET_PORT port is available"
 fi
 
 
@@ -37,10 +46,10 @@ systemctl enable node_exporter
 systemctl restart node_exporter
 
 if systemctl is-active --quiet node_exporter; then
-    echo '[+] Service running! Check http://127.0.0.1:9100'
+    echo "[+] Service running! Check http://127.0.0.1:$TARGET_PORT"
     echo '[+] Dont forget about local firewall if exists!'
     echo '[*] Reminder:'
-    echo '> firewall-cmd --add-port=9100/tcp'
+    echo "> firewall-cmd --add-port=$TARGET_PORT/tcp"
     echo '> firewall-cmd --runtime-to-permanent'
 else
     echo '[-] For unknown reason service didnt started :('
