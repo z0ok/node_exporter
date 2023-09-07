ed259d0a181b159ef3d8b3299ce75f4cce4eb0b4
z0ok <44017357+z0ok@users.noreply.github.com>
2023-09-07 10:56:57 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,10 +1,46 @@
-if id 'node_exporter' &>/dev/null; then
-    echo '[+] User found'
-else
-    echo '[-] Creating user'
-    useradd -rs /bin/false node_exporter
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "[!] Please run as root."
+  exit
 fi
+
+echo '[*] Creating user'
+
+if id 'node_exporter' &>/dev/null; then
+    echo '[+] User already exists'
+else
+    useradd -rs /bin/false node_exporter
+    if id 'node_exporter' &>/dev/null; then
+        echo '[+] User created'
+    else
+        echo '[-] Cannot add user for some reason.'
+        exit
+    fi
+fi
+
+if lsof -Pi :9100 -sTCP:LISTEN -t >/dev/null ; then
+    echo "[-] 9100 port is already used. Cannot bind there!"
+    exit
+else
+    echo "[+] Port is available"
+fi
+
+
+echo '[*] Copying config'
 cp ./node_exporter.service /etc/systemd/system/
+echo '[*] Restarting service'
 systemctl daemon-reload
 systemctl enable node_exporter
 systemctl restart node_exporter
+
+if systemctl is-active --quiet node_exporter; then
+    echo '[+] Service running! Check http://127.0.0.1:9100'
+    echo '[+] Dont forget about local firewall if exists!'
+    echo '[*] Reminder:'
+    echo '> firewall-cmd --add-port=9100/tcp'
+    echo '> firewall-cmd --runtime-to-permanent'
+else
+    echo '[-] For unknown reason service didnt started :('
+    exit
+fi
+
